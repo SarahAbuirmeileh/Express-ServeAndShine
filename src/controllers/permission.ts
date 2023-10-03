91fb30b92c657e95b9aa7104bb461524312b70f9
Sarah Abu irmeileh <211027@ppu.edu.ps>
2023-10-03 11:55:07 +0300

    creat a function to edit the name of permission

--- src/controllers/permission.ts
+++ src/controllers/permission.ts
@@ -39,4 +39,36 @@ const deletePermission = async (permissionId: number, sender: OrganizationAdmin 
 
 }
 
-export { createPermission, deletePermission }
+const editPermission = async (payload: {name:string, id:number}, sender:Volunteer|OrganizationAdmin ) => {
+    const roles = sender.roles;
+    let hasEditPermission: boolean = false;
+    let typeName: string = "";
+
+    if (sender instanceof OrganizationAdmin) {
+        if (!Array.isArray(roles)) {
+            typeName = "organizationAdmin";
+            hasEditPermission = roles.permissions.some(permission => permission.name === `EDIT_${typeName}`);
+        }
+    } else {
+        if (Array.isArray(roles)) {
+            typeName = sender?.type;
+            hasEditPermission = roles?.some(role =>
+                role.permissions.some(permission => permission.name === `DELETE_${typeName}`))
+        }
+    }
+
+    // if (!hasEditPermission) {
+    //     return `You don't have a permission to delete ${typeName}`
+    // }
+
+    const permission = await Permission.findOne({where:{id: payload.id}});
+  
+    if (permission) {
+        permission.name =payload.name;
+        return permission.save();
+    } else {
+      return "Permission not found :(";
+    }
+  }
+
+export { createPermission, deletePermission, editPermission }
