1bf6ca1a87df56ebd3a7ea431a4c8aa91ff78337
Sarah Abu irmeileh <211027@ppu.edu.ps>
2023-10-03 12:33:11 +0300

    creat 2 end point to get user by id and all users

--- src/routes/permission.ts
+++ src/routes/permission.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPermission, deletePermission, editPermission } from '../controllers/permission.js';
+import { createPermission, deletePermission, editPermission, getPermission, getPermissions } from '../controllers/permission.js';
 import { getSender } from '../controllers/index.js';
 
 var router = express.Router();
@@ -14,8 +14,8 @@ router.post('/', (req, res, next) => {
 });
 
 router.delete('/:id', async (req, res) => {
-    const id = Number(req.params.id?.toString()) ;
-    const  sender = await getSender(res);
+    const id = Number(req.params.id?.toString());
+    const sender = await getSender(res);
 
     deletePermission(id, sender)
         .then(data => {
@@ -29,14 +29,41 @@ router.delete('/:id', async (req, res) => {
 
 router.put("/", async (req, res, next) => {
     const sender = await getSender(res);
-  
-    editPermission(req.body,sender).then(() => {        
-      res.status(201).send("Permission edited successfully!!")
+
+    editPermission(req.body, sender).then(() => {
+        res.status(201).send("Permission edited successfully!!")
     }).catch(err => {
-      console.error(err);
-      res.status(500).send(err);
+        console.error(err);
+        res.status(500).send(err);
     });
-  });
+});
+
+router.get("/:id", async (req, res) => {
+    const id = Number(req.params.id)
+
+    const permission = await getPermission({ id });
+    if (permission) {
+        res.status(201).send(permission);
+    } else {
+        res.status(404).send("Permission not found :(");
+    }
+})
+
+router.get('/', async (req, res, next) => {
+    const payload = {
+        page: req.query.page?.toString() || '1',
+        pageSize: req.query.pageSize?.toString() || '10'
+    };
+
+    getPermissions(payload)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Something went wrong');
+        });
+});
 
 export default router;
 
