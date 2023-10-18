3598d5249a107debaf59019634cbc5e3d940c470
TarteelTamimi <TarteelTamimi@gmail.com>
2023-10-18 15:40:51 +0300

    use SES without templates

--- app.ts
+++ app.ts
@@ -16,34 +16,41 @@ import organizationProfileRouter from "./src/routes/organizationProfile.js"
 import volunteerRouter from "./src/routes/volunteer.js"
 import { authenticate } from "./src/middleware/auth/authenticate.js"
 import { errorHandler } from "./src/middleware/errorHandler/errorHandler.js"
+import { sendEmail } from "./src/controllers/sendEmail.js"
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }))
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }))
 
-app.use('/', indexRouter);
-app.use('/permission', authenticate, permissionRouter);
-app.use('/role', authenticate, roleRouter);
-app.use('/voluntaryWork', authenticate, voluntaryWorkRouter);
-app.use('/organizationAdmin', /*authenticate,*/ organizationAdminRouter);
-app.use('/organizationProfile', /*authenticate,*/ organizationProfileRouter);
-app.use("/volunteer", volunteerRouter);
+// app.use('/', indexRouter);
+// app.use('/permission', authenticate, permissionRouter);
+// app.use('/role', authenticate, roleRouter);
+// app.use('/voluntaryWork', authenticate, voluntaryWorkRouter);
+// app.use('/organizationAdmin', /*authenticate,*/ organizationAdminRouter);
+// app.use('/organizationProfile', /*authenticate,*/ organizationProfileRouter);
+// app.use("/volunteer", volunteerRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-app.use(errorHandler);
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+// app.use(errorHandler);
+
+sendEmail(
+'tamimitarteel@gmail.com',
+'Sarah',
+'greeting!',
+'You have registered in bla bla bla voluntary work');
 
 app.listen(PORT, () => {
   logger(`App is listening on port ${PORT}`);
   console.log(`App is listening on port ${PORT} and host http://localhost:${PORT}`);
-  initDB();
+  //initDB();
 });
 
 export default app;
