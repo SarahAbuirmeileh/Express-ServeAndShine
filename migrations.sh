f69a6793b185d9a93722da1d976846d3daede1dd
Sarah Abu irmeileh <127017088+SarahAbuirmeileh@users.noreply.github.com>
2023-10-18 19:09:46 +0300

    Update migrations.sh

--- migrations.sh
+++ migrations.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-until mysqladmin ping -hmysql --silent; do
-    echo "Waiting for MySQL to be ready..."
-    sleep 1
-done
-
 docker-compose -f docker-compose-prod.yml exec app npx typeorm-ts-node-esm migration:run -d ./src/db/dataSource.ts
 docker-compose -f docker-compose-prod.yml exec app mv ./src/db/migrations/1697005494066-CreateRoles.ts ./src/db/migration
 docker-compose -f docker-compose-prod.yml exec app npx typeorm-ts-node-esm migration:run -d ./src/db/dataSource.ts
